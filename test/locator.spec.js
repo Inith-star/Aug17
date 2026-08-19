b768bb6b19c28650d3bcfae36c9250aa94df6d08
Inith-star <inithantony90@gmail.com>
2026-08-19 17:19:36 +0530

    dats

--- test/locator.spec.js
+++ test/locator.spec.js
@@ -1,10 +1,3 @@
 import{test}from'@playwright/test';
 test('Browser launch', async ({page}) => {
 await page. goto('https://www.flipkart.com');})
-
-await page . locator ('')
-await page. goto('https://www.flipkart.com');})
-
-
-
-
