b1d5937f76d0dd6aeb06f94fa60344a3d4179469
Inith-star <inithantony90@gmail.com>
2026-08-19 16:51:14 +0530

    change done

--- test/locator.spec.js
+++ test/locator.spec.js
@@ -1,7 +1,9 @@
 import{test}from'@playwright/test';
 test('Browser launch', async ({page}) => {
 await page. goto('https://www.flipkart.com');})
+
 await page . locator ('')
+await page. goto('https://www.flipkart.com');})
 
 
 
